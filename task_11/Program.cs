96012730ea7753517625f0dd93d38eda10b4c4c6
Дмитрий Иванов <namek85@inbox.ru>
2022-12-08 20:23:14 +0300

    task_11

---
+++ task_11/Program.cs
@@ -0,0 +1,7 @@
+﻿Random random = new Random();
+int number = random.Next (99,1000); //579
+Console.WriteLine("Случайное число = {0} ", number);
+int a = number / 100;
+int b = number % 10;
+
+Console.WriteLine("{0}{1}", a, b);
