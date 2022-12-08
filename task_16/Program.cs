c007ab99f2932050425d217baa873360a708ddba
Дмитрий Иванов <namek85@inbox.ru>
2022-12-08 21:36:15 +0300

    task_16

---
+++ task_16/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите два числа");
+int N = Convert.ToInt32(Console.ReadLine());
+int P = Convert.ToInt32(Console.ReadLine());
+if ((N * N) == P | (P * P) == N)
+ Console.WriteLine("Да");
+ else
+ Console.WriteLine("Нет");
