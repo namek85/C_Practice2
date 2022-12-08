06808eafc314aabfc231a6cd3d26077faf7040b9
Дмитрий Иванов <namek85@inbox.ru>
2022-12-08 19:42:56 +0300

    task11

---
+++ task11/Program.cs
@@ -0,0 +1,11 @@
+﻿Random random = new Random();
+int number = random.Next (10,100);
+Console.WriteLine("Случайное число = {0} ", number);
+int a = number / 10;
+int b = number % 10;
+if (a > b)
+Console.WriteLine("a > b, {0}", a);
+else if (b > a)
+Console.WriteLine("b > a, {0}", b);
+else
+ Console.WriteLine("b == a, {0}", a);
