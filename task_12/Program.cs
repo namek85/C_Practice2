1544facf5917cecdd5002efd3336df8780a19da4
Дмитрий Иванов <namek85@inbox.ru>
2022-12-08 21:09:40 +0300

    task_12

---
+++ task_12/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int P = Convert.ToInt32(Console.ReadLine());
+if ((N % P) == 0)
+ Console.WriteLine("Кратно");
+ else
+ Console.WriteLine("Не кратно " + N%P);
+
